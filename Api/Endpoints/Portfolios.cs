438255523b8a0a166048eb9902bec78a91c4398f
Michal Talaga <michal.talaga@talentclouds.co.uk>
2022-08-28 21:37:33 +0100

    flattened namespace in Portfolio

--- Api/Endpoints/Portfolios.cs
+++ Api/Endpoints/Portfolios.cs
@@ -6,38 +6,37 @@ using Api.Services;
 using static Api.Services.IPortfolioService;
 using System.Net.Http;
 
-namespace Api.Endpoints
+namespace Api.Endpoints;
+
+public class Portfolios
 {
-    public class Portfolios
+    private readonly IPortfolioService portfolioService;
+    private readonly IUserContext userContext;
+
+    public Portfolios(IPortfolioService portfolioService, IUserContext userContext)
     {
-        private readonly IPortfolioService portfolioService;
-        private readonly IUserContext userContext;
+        this.portfolioService = portfolioService;
+        this.userContext = userContext;
+    }
 
-        public Portfolios(IPortfolioService portfolioService, IUserContext userContext)
-        {
-            this.portfolioService = portfolioService;
-            this.userContext = userContext;
-        }
+    [FunctionName(nameof(CreatePortfolio))]
+    public async Task<IActionResult> CreatePortfolio(
+    [HttpTrigger(AuthorizationLevel.Function, "post", Route = "portfolios")] CreatePortfolioModel portfolio)
+    {
+        await portfolioService.Create(userContext.GetEmail(), portfolio);
+        return new StatusCodeResult(StatusCodes.Status201Created);
+    }
+    [FunctionName(nameof(GetPortfolios))]
+    public IEnumerable<Portfolio> GetPortfolios(
+    [HttpTrigger(AuthorizationLevel.Function, "get", Route = "portfolios")] HttpRequestMessage req)
+    {
+        return portfolioService.Get(userContext.GetEmail());
+    }
 
-        [FunctionName(nameof(CreatePortfolio))]
-        public async Task<IActionResult> CreatePortfolio(
-        [HttpTrigger(AuthorizationLevel.Function, "post", Route = "portfolios")] CreatePortfolioModel portfolio)
-        {
-            await portfolioService.Create(userContext.GetEmail(), portfolio);
-            return new StatusCodeResult(StatusCodes.Status201Created);
-        }
-        [FunctionName(nameof(GetPortfolios))]
-        public IEnumerable<Portfolio> GetPortfolios(
-        [HttpTrigger(AuthorizationLevel.Function, "get", Route = "portfolios")] HttpRequestMessage req)
-        {
-            return portfolioService.Get(userContext.GetEmail());
-        }
-
-        [FunctionName(nameof(GetPortfolio))]
-        public Portfolio GetPortfolio(
-        [HttpTrigger(AuthorizationLevel.Function, "get", Route = "portfolios/{name}")] HttpRequestMessage req, string name)
-        {
-            return portfolioService.Get(userContext.GetEmail(), name);
-        }
+    [FunctionName(nameof(GetPortfolio))]
+    public Portfolio GetPortfolio(
+    [HttpTrigger(AuthorizationLevel.Function, "get", Route = "portfolios/{name}")] HttpRequestMessage req, string name)
+    {
+        return portfolioService.Get(userContext.GetEmail(), name);
     }
 }
