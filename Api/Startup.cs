327c702e1d5810f1d25580724a453ff4cd002ffa
Michal Talaga <michal.talaga@talentclouds.co.uk>
2022-09-04 20:47:45 +0100

    Added custom CosmosDbSerializer to serialize enums as strings

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -1,9 +1,13 @@
 ﻿using Api;
 using Api.Infrastructure;
 using Api.Services;
+using Api.UglyStuff;
 using Microsoft.Azure.Cosmos.Fluent;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Text.Json.Serialization;
 
 [assembly: FunctionsStartup(typeof(Startup))]
 namespace Api;
@@ -14,6 +18,13 @@ public class Startup : FunctionsStartup
     {
         var connectionString = Environment.GetEnvironmentVariable("StockerCosmosDB") ?? Environment.GetEnvironmentVariable("ConnectionStrings:StockerCosmosDB");
         var cosmosClientBuilder = new CosmosClientBuilder(connectionString);
+        cosmosClientBuilder.WithCustomSerializer(new CosmosDbSerializer(new System.Text.Json.JsonSerializerOptions
+        {
+            Converters =
+            {
+                new JsonStringEnumConverter()
+            }
+        }));
         var cosmosClient = cosmosClientBuilder.Build();
 
         builder.Services.AddTransient(services => cosmosClient.GetDatabase("Stocker"));
---
+++ Api/UglyStuff/CosmosDbSerializer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.UglyStuff
+{
+    using System.IO;
+    using System.Text.Json;
+    using Azure.Core.Serialization;
+    using Microsoft.Azure.Cosmos;
+
+    /// <remarks>
+    // See: https://github.com/Azure/azure-cosmos-dotnet-v3/blob/master/Microsoft.Azure.Cosmos.Samples/Usage/SystemTextJson/CosmosSystemTextJsonSerializer.cs
+    /// </remarks>
+    public sealed class CosmosDbSerializer : CosmosSerializer
+    {
+        private readonly JsonObjectSerializer systemTextJsonSerializer;
+
+        public CosmosDbSerializer() : this(new JsonSerializerOptions())
+        {
+
+        }
+        public CosmosDbSerializer(JsonSerializerOptions jsonSerializerOptions)
+        {
+            systemTextJsonSerializer = new JsonObjectSerializer(jsonSerializerOptions);
+        }
+
+        public override T FromStream<T>(Stream stream)
+        {
+            if (stream.CanSeek && stream.Length == 0)
+            {
+                return default;
+            }
+
+            if (typeof(Stream).IsAssignableFrom(typeof(T)))
+            {
+                return (T)(object)stream;
+            }
+
+            using (stream)
+            {
+                return (T)systemTextJsonSerializer.Deserialize(stream, typeof(T), default);
+            }
+        }
+
+        public override Stream ToStream<T>(T input)
+        {
+            var streamPayload = new MemoryStream();
+            systemTextJsonSerializer.Serialize(streamPayload, input, typeof(T), default);
+            streamPayload.Position = 0;
+            return streamPayload;
+        }
+    }
+}
